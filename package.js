f6024cbb7c6e2f41b65fecd7ad3fecf217c66543
Mitar <mitar.git@tnode.com>
2018-05-08 00:11:29 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'tozd:activity-instrument',
   summary: "Instrument client and server to log user activity.",
-  version: '0.1.2',
+  version: '0.1.3',
   git: 'https://github.com/tozd/meteor-activity-instrument.git'
 });
 
